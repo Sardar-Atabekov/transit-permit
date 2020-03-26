74faa38c5690227e540fbc78d1bde83321f084be
Sardar Atavekov <sardarbekatabekov@gmail.com>
2020-03-26 13:10:29 +0600

    build

---
+++ build/precache-manifest.293051fe206ae7486af9b34dd6a01238.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f1b288a2bb82f5703e55954c05af61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c61e2eb1f05a3d94e24",
+    "url": "/static/css/2.ebe46193.chunk.css"
+  },
+  {
+    "revision": "7547001c750bfbf61d17",
+    "url": "/static/css/main.83cf2f4a.chunk.css"
+  },
+  {
+    "revision": "0c61e2eb1f05a3d94e24",
+    "url": "/static/js/2.40321b15.chunk.js"
+  },
+  {
+    "revision": "c8274d3be23fa310adf3ffdb9896c079",
+    "url": "/static/js/2.40321b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7547001c750bfbf61d17",
+    "url": "/static/js/main.8597d7a3.chunk.js"
+  },
+  {
+    "revision": "4d9af2a9620265095b18",
+    "url": "/static/js/runtime-main.e8bda96d.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
